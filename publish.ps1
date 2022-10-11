fc3ae8faf69d6cdac2425bbe523b9f030ee9c897
Sardor <skhsafarov@edu.hse.ru>
2022-10-11 21:45:26 +0300

    saved main branch

--- publish.ps1
+++ publish.ps1
@@ -3,9 +3,9 @@ git add *
 git commit -m "saved main branch"
 git push
 move bin/Release/net6.0/Block/.git/ bin/Release/net6.0/
-remove-item -path "$bin/Release/net6.0/Block/*" -recurse
+remove-item -path bin/Release/net6.0/Block/* -recurse
 move bin/Release/net6.0/.git/ bin/Release/net6.0/Block/
-remove-item -path "$bin/Release/net6.0/Published/*" -recurse
+remove-item -path bin/Release/net6.0/Published/* -recurse
 dotnet publish -c Release -o bin/Release/net6.0/Published/
 cd bin/Release/net6.0/Block
 move ../Published/wwwroot/*
