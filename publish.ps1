169f52c53335ccdbd3fbadbe0201b5501e1bb5ce
Sardor <skhsafarov@edu.hse.ru>
2022-10-11 22:09:12 +0300

    saved main branch

--- publish.ps1
+++ publish.ps1
@@ -2,15 +2,16 @@
 git add *
 git commit -m "saved main branch"
 git push
-move bin/Release/net6.0/Block/.git/ bin/Release/net6.0/
+move bin/Release/net6.0/Block/.git bin/Release/net6.0
 remove-item -path bin/Release/net6.0/Block/* -recurse
-move bin/Release/net6.0/.git/ bin/Release/net6.0/Block/
+move bin/Release/net6.0/.git bin/Release/net6.0/Block
 remove-item -path bin/Release/net6.0/Published/* -recurse
-dotnet publish -c Release -o bin/Release/net6.0/Published/
+dotnet publish -c Release -o bin/Release/net6.0/Published
 cd bin/Release/net6.0/Block
 move ../Published/wwwroot/*
 git checkout gh-pages
 git add *
 git commit -m "published gh-pages branch"
 git push
-powershell.exe -NoExit D:/Block/publish.ps1
+
+powershell.exe -NoExit D:\Block\publish.ps1
