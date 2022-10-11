778ea0189ac353d7c1c9502257bc0106b316783e
Sardor <skhsafarov@edu.hse.ru>
2022-10-11 20:53:24 +0300

    saved main branch

--- publish.ps1
+++ publish.ps1
@@ -1,5 +1,19 @@
 ﻿git add *
 git commit -m "saved main branch"
 git push
+move /bin/Release/net6.0/Block/.git/ /bin/Release/net6.0/
+rm /bin/Release/net6.0/Block/*
+A
+move /bin/Release/net6.0/.git/ /bin/Release/net6.0/Block/
+rm /bin/Release/net6.0/Published/*
+A
+dotnet publish -c Release -o /bin/Release/net6.0/Published/
+
 cd /bin/Release/net6.0/Block/
-cd /bin/Release/net6.0/publish/
+move ../Published/wwwroot/*
+git add *
+git commit -m "published gh-pages branch"
+git push
+
+
+cd /bin/Release/net6.0/publish/sax
